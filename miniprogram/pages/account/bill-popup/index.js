e185241c05ba9097f9b778a01c02bac3e9e6e3d9
jswxwxf <wxwxf@hotmail.com>
2025-07-29 10:57:19 +0800

    feat: 实现记账弹窗表格界面和数据绑定

--- miniprogram/pages/account/bill-popup/index.js
+++ miniprogram/pages/account/bill-popup/index.js
@@ -3,6 +3,13 @@ import { defineComponent } from '@vue-mini/core'
 defineComponent({
   data: {
     visible: false,
+    list: Array.from({ length: 5 }, () => ({
+      date: '',
+      category: '',
+      amount: '',
+      note: '',
+      tags: '',
+    })),
   },
   methods: {
     show() {
@@ -10,6 +17,13 @@ defineComponent({
     },
     handleClose() {
       this.setData({ visible: false })
+    },
+    handleFormChange(e) {
+      const { rowIndex, field } = e.currentTarget.dataset
+      const { value } = e.detail
+      this.setData({
+        [`list[${rowIndex}].${field}`]: value,
+      })
     },
   },
 })
