60acf0e487a7fabbc371f02b0b16654ab8ec0fdd
jswxwxf <wxwxf@hotmail.com>
2025-08-10 07:27:21 +0800

    feat: 实现设置页面基本布局和清空账目功能

--- miniprogram/pages/setting/index.js
+++ miniprogram/pages/setting/index.js
@@ -1,1 +1,24 @@
-Component({})
+import { defineComponent } from '@vue-mini/core'
+import Dialog from '@vant/weapp/dialog/dialog';
+
+defineComponent({
+  setup() {
+    const handleReset = () => {
+      Dialog.confirm({
+        title: '确认清空帐目',
+        message: '此操作将清空所有记账数据，并无法恢复。确定要重新开始吗？',
+        confirmButtonText: '确认清空',
+        confirmButtonColor: '#ee0a24',
+      }).then(() => {
+        console.log('User confirmed to reset account book.')
+        // 在这里添加清空数据的逻辑
+      }).catch(() => {
+        // on cancel
+      });
+    }
+
+    return {
+      handleReset,
+    }
+  },
+})
