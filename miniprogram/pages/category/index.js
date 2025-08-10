a098663308d93fe6dc848e242c0a79796447babc
jswxwxf <wxwxf@hotmail.com>
2025-08-10 08:35:13 +0800

    feat: 调用云函数获取分类数据

--- miniprogram/pages/category/index.js
+++ miniprogram/pages/category/index.js
@@ -1,25 +1,33 @@
-import { defineComponent, ref, reactive } from '@vue-mini/core'
+import { defineComponent, ref, onShow } from '@vue-mini/core'
+import { getCategories } from '@/api/category.js'
+import { groupBy } from 'lodash'
 
 defineComponent({
   setup() {
     const activeTab = ref(0)
-    const categories = reactive({
-      expense: [
-        '餐饮', '交通', '服饰', '购物', '服务', '教育', '娱乐', '运动',
-        '生活缴费', '旅行', '宠物', '医疗', '保险', '公益', '发红包', '转账',
-        '餐饮', '交通', '服饰', '购物', '服务', '教育', '娱乐', '运动',
-        '生活缴费', '旅行', '宠物', '医疗', '保险', '公益', '发红包', '转账',
-        '餐饮', '交通', '服饰', '购物', '服务', '教育', '娱乐', '运动',
-        '生活缴费', '旅行', '宠物', '医疗', '保险', '公益', '发红包', '转账'
-      ],
-      income: [
-        '工资', '兼职', '理财', '红包', '其他'
-      ]
+    const categories = ref({
+      expense: [],
+      income: [],
     })
 
     const onTabChange = (event) => {
       activeTab.value = event.detail.index
     }
+
+    async function loadData() {
+      const res = await getCategories()
+      const grouped = groupBy(res.data, 'type')
+      categories.value = {
+        expense: grouped['20'] || [],
+        income: grouped['10'] || [],
+      }
+    }
+
+    loadData()
+
+    onShow(() => {
+      loadData()
+    })
 
     return {
       activeTab,
