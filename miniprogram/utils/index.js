78bc8478c89237417449f8006ea0e5a083e25e0e
jswxwxf <wxwxf@hotmail.com>
2025-09-12 11:11:45 +0800

    fix: 修复 deprecated 问题，使用slice替代已弃用的substr方法

--- miniprogram/utils/index.js
+++ miniprogram/utils/index.js
@@ -196,5 +196,5 @@ export function deepCopy(obj) {
  * @returns {string} - 生成的 ID
  */
 export function generateId(prefix = '') {
-  return prefix + Date.now().toString(36) + Math.random().toString(36).substr(2)
+  return prefix + Date.now().toString(36) + Math.random().toString(36).slice(2)
 }
